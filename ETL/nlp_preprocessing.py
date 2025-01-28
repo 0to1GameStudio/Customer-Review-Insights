271ab6c92860ab7e7a026a099cca9cb9da3ccfe0
0to1gamestudio <0to1gamestudio@gmail.com>
2025-01-28 21:05:03 +0530

    main commit

---
+++ ETL/nlp_preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+print('Testing')
+    
+#input_file = r"D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\Data\Datasete-SA.csv"
+#df = pd.read_csv(r'D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\Data\Datasete-SA.csv')
+#df.head()
+
+#setting up or loading csv file into python environment.
+class nlp:
+     def __init__(self,filename):
+        self.filename = file_name
+        
+     def extract(self):
+        df = pd.read_csv(self.filename)
+        return df.head()
+        
+
+
+file_name= r'D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\data\Dataset-SA.csv'
+data_reader = nlp(file_name)
+datas = data_reader.extract()
+print(datas)
+
+
+
