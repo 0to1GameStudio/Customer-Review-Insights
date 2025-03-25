6b4c0b700d778643097d71cea352d54e1a83af29
0to1gamestudio <0to1gamestudio@gmail.com>
2025-03-25 23:23:25 +0530

    Newly updated changes

--- ETL/database_data.py
+++ ETL/database_data.py
@@ -29,7 +29,11 @@ class CSV_SQL:
                 product_dim = df[selected_columns_prod_dim]
                 review_summary_dim = df[selected_columns_review_sum]
 
+                review_summary_dim = review_summary_dim.drop_duplicates(subset=['review'])
+                product_dim = product_dim.drop_duplicates(subset=['product_name'])
+                
                 product_dim['product_id'] = range(1, len(product_dim) + 1)
+
                 review_summary_dim['review_summary_id'] = range(1,len(review_summary_dim) + 1)
                 final_sel_data_prod = ['product_id','product_name','product_price']
                 final_sel_data_rev_sum = ['review_summary_id','review','summary']
@@ -48,17 +52,18 @@ class CSV_SQL:
                 print('Read the table dim_review_summary from the database.')
                 print(df_reviews_sum.head())
                 
-                fact_mid_reviews = pd.concat([df_products,df_reviews_sum,fact_reviews],axis=1)
+                # fact_mid_reviews = pd.concat([df_products,df_reviews_sum,fact_reviews],axis=1)
+                fact_mid_reviews = fact_reviews.merge(df_products, on="product_name", how="left")\
+                    .merge(df_reviews_sum, on="review", how="left")
+
                 fact_mid_reviews['review_id'] = range(1,len(fact_mid_reviews) +  1)
 
                 selected_columns_fact_t = ['review_id','product_id','review_summary_id','rate','sentiment']
                 fact_mid_reviews = fact_mid_reviews[selected_columns_fact_t]
-                # print('\n Fact Table : \n',fact_mid_reviews.iloc[40:50],'\n',fact_mid_reviews.info())
                
-                fact_mid_reviews.to_sql('fact_reviews_product_summary',conn,if_exists='replace',index=False)
-                print('Successfully Loaded Fact Table records into database.')
-                data_fact = pd.read_sql('select * from fact_reviews_product_summary',conn)
-                print('\n',data_fact.head(20))
+                # fact_mid_reviews.to_sql('fact_reviews_product_summary',conn,if_exists='replace',index=False,chunksize=4000)
+                print('\n',fact_mid_reviews.iloc[20:40])
+                # print('Successfully Loaded Fact Table records into database.')
 
             else:
                 print('File doesn''t present in folder.')
@@ -73,6 +78,6 @@ class CSV_SQL:
                 print('Database Disconnected.')
 
 if __name__ == '__main__':
-    in_file = r'D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\data\Processed_Dataset.csv'
-    convert = CSV_SQL(in_file)
-    convert.csv_to_sql()
+    processed_file = r'D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\data\Processed_Dataset.csv'
+    db_loader = CSV_SQL(processed_file)
+    db_loader.csv_to_sql()
--- ETL/nlp_preprocessing.py
+++ ETL/nlp_preprocessing.py
@@ -3,7 +3,7 @@ from database_data import CSV_SQL
 
 print('NLP Pre-Processing...')
 # Setting up or loading CSV file into Python environment.
-class nlp:
+class nlp_ETL:
     def __init__(self,input_filename,output_filename):
         self.input_filename = input_filename
         self.output_filename = output_filename
@@ -38,9 +38,16 @@ class nlp:
             print('\n Counting Ratings for each product. \n')
             print(self.data['Rate'].value_counts())
 
-            self.data = self.data[(self.data.Rate !='Pigeon Favourite Electric Kettle??????(1.5 L, Silver, Black)') 
-                    & (self.data.Rate != "Bajaj DX 2 L/W Dry Iron") 
-                    & (self.data.Rate !='Nova Plus Amaze NI 10 1100 W Dry Iron?ÃÂ¿?ÃÂ¿(Grey & Turquoise)')]
+            noisy_values = [
+                'Pigeon Favourite Electric Kettle??????(1.5 L, Silver, Black)',
+                "Bajaj DX 2 L/W Dry Iron",
+                'Nova Plus Amaze NI 10 1100 W Dry Iron?ÃÂ¿?ÃÂ¿(Grey & Turquoise)'
+            ]
+            self.data = self.data[~self.data['Rate'].isin(noisy_values)]
+
+            # self.data = self.data[(self.data.Rate !='Pigeon Favourite Electric Kettle??????(1.5 L, Silver, Black)') 
+            #         & (self.data.Rate != "Bajaj DX 2 L/W Dry Iron") 
+            #         & (self.data.Rate !='Nova Plus Amaze NI 10 1100 W Dry Iron?ÃÂ¿?ÃÂ¿(Grey & Turquoise)')]
             
             print('\n As unexpectedly we got noisy data or irrelevant information that can negatively impact data analysis.\n'
             'Then deleted those values or rows in dataset.')
@@ -60,7 +67,8 @@ class nlp:
             path_to = 'Customer Review Insights Project -> Project -> data -> Processed_Dataset.csv'
             print('\nSuccessfully Data Loaded to Target Destination / Location. with location: ',path_to)
             print('Updating those records or data to the database - PostgreSQL',self.data.shape)
-            CSV_SQL(self.data).csv_to_sql()
+            csv_loader = CSV_SQL(self.output_filename)
+            csv_loader.csv_to_sql()
         else:
             print('No Data is to Load. ')
     
@@ -75,6 +83,5 @@ if __name__ == '__main__':
     in_file_name = r'D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\data\Dataset-SA.csv'
     out_filename = r'D:\NikhilData\Desktop\Webpages\Python Practice\Customer Review Insights Project\Project\data\Processed_Dataset.csv'
 
-    ETL = nlp(in_file_name,out_filename)
+    ETL = nlp_ETL(in_file_name,out_filename)
     ETL.run()
-
